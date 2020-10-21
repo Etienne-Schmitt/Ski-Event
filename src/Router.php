710876e89e6656121958b6c39a078201219e5196
Etienne Schmitt <etienne@schmitt-etienne.fr>
2020-10-21 15:08:32 +0200

    Create \Syrgoma\Ski\Router->createNewRoute() for add a new route

--- src/Router.php
+++ src/Router.php
@@ -1,6 +1,14 @@
 <?php
 
+/**
+ * @author  Etienne Schmitt <etienne@schmitt-etienne.fr>
+ * @license GPL2 or any later version
+ */
+
 namespace Syrgoma\Ski;
+
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
 
 /**
  * Class Router
@@ -17,6 +25,9 @@ class Router
      * @var Router|null contain the router instance
      */
     protected static ?Router $routerInstance = null;
+
+    /** @var RouteCollection|null contain the list of routes */
+    protected ?RouteCollection $routeCollection = null;
 
     /**
      * __construct() protected for singleton class
@@ -54,5 +65,47 @@ class Router
      */
     protected function __clone()
     {
+    }
+
+    /**
+     * Return the route list in singleton style
+     *
+     * @return RouteCollection
+     */
+    protected function getRouteCollection(): RouteCollection
+    {
+        if ($this->routeCollection === null) {
+            $this->routeCollection = new RouteCollection();
+        }
+        return $this->routeCollection;
+    }
+
+    /**
+     * This add a new route to the router
+     *
+     * @param string     $controllerName     name of the controller used
+     * @param string     $routeName          name of the route for debugging
+     * @param string     $path               path to match
+     * @param array|null $otherRouteSettings other settings
+     *
+     * @example $route->createNewRoute(TestController::class, "test_route", "/test")
+     */
+    public function createNewRoute(
+        string $controllerName,
+        string $routeName,
+        string $path,
+        array $otherRouteSettings = null
+    ): void {
+        $newRoute = new Route($path, [
+            "_controller" => $controllerName
+        ]);
+
+        if ($otherRouteSettings !== null) {
+            foreach ($otherRouteSettings as $optionName => $optionValue) {
+                $newRoute->addOptions([$optionName => $optionValue]);
+            }
+        }
+
+        $this->getRouteCollection()->add($routeName, $newRoute);
     }
 }
