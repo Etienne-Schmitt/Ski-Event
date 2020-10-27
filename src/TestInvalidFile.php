a79c93bb7b274b194b8edf72253cc3b7bc0c3772
Etienne Schmitt <etienne@schmitt-etienne.fr>
2020-10-27 15:49:37 +0100

    Add bad file for testing github actions

---
+++ src/TestInvalidFile.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @author  Etienne Schmitt <etienne@schmitt-etienne.fr>
+ * @license GPL 2.0 or any later
+ */
+
+class invalidForTest
+{
+
+    public function TESTFONCTION()
+    {
+
+    }
+
+}
+
+$test = "42";
+
+
+if ($test == 42) {
+    die();
+}
