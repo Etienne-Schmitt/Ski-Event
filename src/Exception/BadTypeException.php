31dcacb224006194977b1f838d69b6beba7f5630
Etienne Schmitt <etienne@schmitt-etienne.fr>
2020-10-31 14:21:14 +0100

    Create Exception used in Epreuve::class

---
+++ src/Exception/BadTypeException.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @author  Etienne Schmitt <etienne@schmitt-etienne.fr>
+ * @license GPL 2.0 or any later
+ */
+
+namespace Syrgoma\Ski\Exception;
+
+use RuntimeException;
+
+class BadTypeException extends RuntimeException
+{
+
+}
