47e886bd09dcb46b36da311eaa4c888af0e41fca
Etienne Schmitt <etienne@schmitt-etienne.fr>
2020-11-01 16:07:09 +0100

    Create CategorieFactory for centralize creation of Categorie object

---
+++ src/Factory/CategorieFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @author  Etienne Schmitt <etienne@schmitt-etienne.fr>
+ * @license GPL 2.0 or any later
+ */
+
+namespace Syrgoma\Ski\Factory;
+
+use Syrgoma\Ski\Entity\Categorie;
+
+class CategorieFactory
+{
+    public static function categorieFactory(int $categorieId, string $categorieName): Categorie
+    {
+        return new Categorie($categorieId, $categorieName);
+    }
+}
