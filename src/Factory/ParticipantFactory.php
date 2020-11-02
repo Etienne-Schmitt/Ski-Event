6f70489abd4f8c92bfadc3d02e86f47628c00678
Etienne Schmitt <etienne@schmitt-etienne.fr>
2020-11-02 01:22:21 +0100

    Create a factory for Participant

---
+++ src/Factory/ParticipantFactory.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * @author  Etienne Schmitt <etienne@schmitt-etienne.fr>
+ * @license GPL 2.0 or any later
+ */
+
+namespace Syrgoma\Ski\Factory;
+
+use Carbon\Carbon;
+use Exception;
+use Syrgoma\Ski\Database\DatabaseConnection;
+use Syrgoma\Ski\Entity\Categorie;
+use Syrgoma\Ski\Entity\Participant;
+use Syrgoma\Ski\Entity\Profil;
+
+class ParticipantFactory
+{
+    /**
+     * @param array              $participant
+     * @param DatabaseConnection $db
+     *
+     * @return Participant
+     * @throws Exception
+     */
+    public static function buildNewParticipantFromFactory(array $participant, DatabaseConnection $db): Participant
+    {
+
+        $categorieParticipantSql     = "SELECT categorie_id FROM participants_categorie WHERE participant_id = ?";
+        $categorieParticipantRequest = $db->obtainOneElementSQLWithParams(
+            $categorieParticipantSql,
+            [$participant['id']]
+        );
+
+        $categorieSql     = "SELECT * FROM categories WHERE id = ?";
+        $categorieRequest = $db->obtainOneElementSQLWithParams(
+            $categorieSql,
+            [$categorieParticipantRequest['categorie_id']]
+        );
+        $categorie        = new Categorie($categorieRequest['id'], $categorieRequest['nom']);
+
+        $profilParticipantSql     = "SELECT profil_id FROM participants_profil WHERE participant_id = ?";
+        $profilParticipantRequest = $db->obtainOneElementSQLWithParams(
+            $profilParticipantSql,
+            [$participant['id']]
+        );
+
+        $profilSql     = "SELECT * FROM categories WHERE id = ?";
+        $profilRequest = $db->obtainOneElementSQLWithParams(
+            $profilSql,
+            [$profilParticipantRequest['profil_id']]
+        );
+        $profil        = new Profil($profilRequest['id'], $profilRequest['nom']);
+
+        return new Participant(
+            $participant['id'],
+            $participant['nom'],
+            $participant['prenom'],
+            new Carbon($participant['date_de_naissance']),
+            $participant['email'],
+            $participant['photo_chemin'],
+            $categorie,
+            $profil
+        );
+    }
+}
